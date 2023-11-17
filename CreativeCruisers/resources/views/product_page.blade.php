a5a2fdc2e5dc0dfd66f711de4f858dc36504aec5
DragosStefanNecula220035325 <148204781+DragosStefanNecula220035325@users.noreply.github.com>
2023-11-17 14:27:04 +0000

    Added Product Layout Page v01

---
+++ CreativeCruisers/resources/views/product_page.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/css/app.css "/> 
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <title>Document</title> 
+</head>
+<body>
+
+    <div id="product_page_header">
+
+            <div id="product_page_container">
+                <img class = "skateboardpic" src="/images/pexels-artem-podrez-4816757.jpg" alt="Skateboards">
+                <div class="centered">
+                    <p class="smalltxt">Home > Product Page</p>
+                    <h1 class="largetxt">Shop Page</h1>
+                    <p class="mediumtxt">Let's build the skateboard you have always imagined.</p>
+                </div>
+              </div>
+
+    </div>
+
+    <div id="product_page_interface">
+        <div id="product_page_interface_body">
+            <div id="product_page_interface_header">
+                
+            </div>
+            <div id="product_page_interface_list">
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+                <div class="product"></div>
+            </div>
+            <div id="product_page_interface_footer"></div>
+                
+            </div>
+        </div>
+
+    </div>
+
+    
+</body>
+</html>
