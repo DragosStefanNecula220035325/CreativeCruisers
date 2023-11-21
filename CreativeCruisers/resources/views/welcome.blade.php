a80d1ba970d2e49cd658965ca19fbaad49c1d18c
GurdevBopps <210073858@aston.ac.uk>
2023-11-21 11:53:24 +0000

    changed main.css to welcome_page.css

--- CreativeCruisers/resources/views/welcome.blade.php
+++ CreativeCruisers/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/welcome_page.css">
 
     
     </head>
