d4c58147858bcabfe00a7687624815a3389da92b
GurdevBopps <210073858@aston.ac.uk>
2023-11-23 17:16:15 +0000

    added basket icon
    added a basket icon and styled the icon as well as a basket counter to go next to it

--- CreativeCruisers/resources/views/welcome.blade.php
+++ CreativeCruisers/resources/views/welcome.blade.php
@@ -1,99 +1,123 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-        <link rel="stylesheet" href="css/welcome_page.css">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    
-    </head>
+    <title>Laravel</title>
+    <link rel="stylesheet" href="css/welcome_page.css">
+
+
+</head>
 
 <body>
 
 
-<div class="header">
+    <div class="header">
 
-    <div class="navbar">
-    
-        <div class="logo">
-            <img src="images/logo.png" width="125px">
+        <div class="navbar">
+
+            <div class="logo">
+                <img src="images/logo.png" width="125px">
+            </div>
+
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="product_page">Products</a></li>
+                    <li><a href=""><ion-icon name="basket-outline"></ion-icon></a><span class="basket_count">0</span>
+                    </li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </nav>
+
+
         </div>
-    
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="product_page">Products</a></li>
-                <li><a href="">About Us</a></li>
-                <li><a href="">Contact Us</a></li>
-    
-    
-            </ul>
-        </nav>
-    
+
+        <div class="row">
+            <div class="col-1">
+                <h1>Create your own SkateBoard!</h1>
+                <p>Make your skateboard your own, with customisation like never before!</p>
+                <a href="" class="btn">Try it now! &#8594;</a>
+            </div>
+        </div>
+
     </div>
 
-    <div class="row">
-        <div class="col-1">
-            <h1>Create your own SkateBoard!</h1>
-            <p>Make your skateboard your own, with customisation like never before!</p>
-            <a href="" class="btn">Try it now! &#8594;</a>
+
+
+    <div class="categories">
+        <div class="row_categories">
+            <div class="category_column">
+                <img src="https://via.placeholder.com/200x150" alt="Decks Image">
+                <h3>Decks</h3>
+            </div>
+            <div class="category_column">
+                <img src="https://via.placeholder.com/200x150" alt="Wheels Image">
+                <h3>Wheels</h3>
+            </div>
+            <div class="category_column">
+                <img src="https://via.placeholder.com/200x150" alt="Trucks Image">
+                <h3>Trucks</h3>
+            </div>
+        </div>
+
+        <h2>Featured Products</h2>
+
+        <div class="row_featured_products">
+            <div class="featured_products">
+                <img src="https://via.placeholder.com/200x150" alt="Product1 Image">
+                <h4>Product1</h4>
+            </div>
+            <div class="featured_products">
+                <img src="https://via.placeholder.com/200x150" alt="Product2 Image">
+                <h4>Product2</h4>
+            </div>
+            <div class="featured_products">
+                <img src="https://via.placeholder.com/200x150" alt="Product3 Image">
+                <h4>Product3</h4>
+            </div>
+            <div class="featured_products">
+                <img src="https://via.placeholder.com/200x150" alt="Product4 Image">
+                <h4>Product4</h4>
+            </div>
+            <div class="featured_products">
+                <img src="https://via.placeholder.com/200x150" alt="Product5 Image">
+                <h4>Product5</h4>
+            </div>
+            <div class="featured_products">
+                <img src="https://via.placeholder.com/200x150" alt="Product6 Image">
+                <h4>Product6</h4>
+            </div>
         </div>
     </div>
 
-</div>
-
-
-
-<div class="categories">
-    <div class="row_categories">
-        <div class="category_column">
-            <img src="https://via.placeholder.com/200x150" alt="Decks Image">
-            <h3>Decks</h3>
-        </div>
-        <div class="category_column">
-            <img src="https://via.placeholder.com/200x150" alt="Wheels Image">
-            <h3>Wheels</h3>
-        </div>
-        <div class="category_column">
-            <img src="https://via.placeholder.com/200x150" alt="Trucks Image">
-            <h3>Trucks</h3>        
-        </div>
-    </div>
-
-    <h2>Featured Products</h2>
-
-    <div class="row_featured_products">
-        <div class="featured_products">
-            <img src="https://via.placeholder.com/200x150" alt="Product1 Image">
-            <h4>Product1</h4>
-        </div>
-        <div class="featured_products">
-            <img src="https://via.placeholder.com/200x150" alt="Product2 Image">
-            <h4>Product2</h4>
-        </div>
-        <div class="featured_products">
-            <img src="https://via.placeholder.com/200x150" alt="Product3 Image">
-            <h4>Product3</h4>
-        </div>
-        <div class="featured_products">
-            <img src="https://via.placeholder.com/200x150" alt="Product4 Image">
-            <h4>Product4</h4>
-        </div>
-        <div class="featured_products">
-            <img src="https://via.placeholder.com/200x150" alt="Product5 Image">
-            <h4>Product5</h4>
-        </div>
-        <div class="featured_products">
-            <img src="https://via.placeholder.com/200x150" alt="Product6 Image">
-            <h4>Product6</h4>
-        </div>
-    </div>
-</div>
-
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 </body>
 
-    
+
 </html>
+
+<script>
+    const basketIcon = document.querySelector('.basket_icon');
+    const basketCount = document.querySelector('.basket_count');
+
+    let itemCount = 0;
+
+    function updateBasketCount() {
+        basketCount.textContent = itemCount;
+    }
+
+    const addToBasketButtons = document.querySelectorAll('.checkout');
+    addToBasketButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            itemCount++;
+            updateBasketCount();
+        })
+
+    });
+
+</script>
