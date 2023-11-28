7a6f6623ac2d8171358ec3cd47efed812216b986
GurdevBopps <210073858@aston.ac.uk>
2023-11-28 11:36:06 +0000

    Added basket icon and link to checkout page
    Added the ionicon script to the header so that the basket image is always present as well as linked the checkout page so that when clicked the user is directed to checkout.

--- CreativeCruisers/resources/views/header.blade.php
+++ CreativeCruisers/resources/views/header.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,64 +11,72 @@
     <title>Creative Cruisers</title>
 
 </head>
+
 <body>
-    
-<link rel="stylesheet" href="css/general.css">
-<link rel="stylesheet" href="css/nav_bar.css">
-<link rel="stylesheet" href="css/components.css">
-<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-<div class="header">
 
-<div class="navbar">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/nav_bar.css">
+    <link rel="stylesheet" href="css/components.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <div class="header">
 
-    <div class="logo">
-        <img src="images/creative_logo.png" width="125px">
-        
-    </div>
-    <div class="title">
-        <img src="images/creative_cruisers_title.png" width="350px" style="margin-left: 50px">
-    </div>
+        <div class="navbar">
 
-    <nav>
-        <ul>
-            <li><a href="welcome">Home</a></li>
-            <li><a href="product_page">Products</a></li>
-            <li><a href="">Contact Us</a></li>
-            @guest
-            @if (Route::has('login'))
-            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-            @endif
-            @if (Route::has('register'))
-            <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-            @endif
-            @else
-            <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }}</a>
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown"> 
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}</a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
+            <div class="logo">
+                <img src="images/creative_logo.png" width="125px">
+
             </div>
-            </li>
-            @endguest
+            <div class="title">
+                <img src="images/creative_cruisers_title.png" width="350px" style="margin-left: 50px">
+            </div>
+
+            <nav>
+                <ul>
+                    <li><a href="welcome">Home</a></li>
+                    <li><a href="product_page">Products</a></li>
+                    <li><a href="">Contact Us</a></li>
+                    @guest
+                    @if (Route::has('login'))
+                    <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                    @endif
+                    @if (Route::has('register'))
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                    @endif
+                    @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}</a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
 
 
 
-            <li><a href=""><ion-icon name="basket-outline"></ion-icon></a><span class="basket_count">0</span>
-            </li>
-        </ul>
-    </nav>
+                    <li><a href="checkout"><ion-icon name="basket-outline"></ion-icon></a><span
+                            class="basket_count">0</span>
+                    </li>
+                </ul>
+            </nav>
 
 
-</div>
+        </div>
 
-@yield('content')
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+        @yield('content')
 </body>
+
 </html>
-    <!-- <div id="app">
+<!-- <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
@@ -79,14 +88,14 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-Left Side Of Navbar -->
-                    <!-- <ul class="navbar-nav me-auto">
+<!-- <ul class="navbar-nav me-auto">
 
                     </ul> -->
 
-                    <!-- Right Side Of Navbar -->
-                    <!-- <ul class="navbar-nav ms-auto"> -->
-                        <!-- Authentication Links -->
-                        <!-- @guest
+<!-- Right Side Of Navbar -->
+<!-- <ul class="navbar-nav ms-auto"> -->
+<!-- Authentication Links -->
+<!-- @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -127,4 +136,4 @@
         </main>
     </div> -->
 <!-- </body>
-</html> --> 
+</html> -->
