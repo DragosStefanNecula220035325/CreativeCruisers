00b02f4e7bdad73856588c85bca2cf5412fc7619
GurdevBopps <210073858@aston.ac.uk>
2023-12-05 19:35:48 +0000

    just formatting

--- CreativeCruisers/resources/views/product_page.blade.php
+++ CreativeCruisers/resources/views/product_page.blade.php
@@ -1,23 +1,15 @@
 @extends('header')
 @section('content')
 <link rel="stylesheet" type="text/css" href="css/product_page.css " />
+
 <div class="row">
     <div class="col-1">
-        <p class="smalltxt"><a href="welcome" class="homeLink">Home</a> > Product Page</p>
-        <h1 class="largetxt">Shop Page</h1>
-        <p class="mediumtxt">Let's build the skateboard you have always imagined.</p>
+        <p> <a href="welcome" class="homeLink">Home</a> > Product Page</p>
+        <h1>Shop Page</h1>
+        <p>Let's build the skateboard you have always imagined.</p>
     </div>
 </div>
-<!-- <div id="product_page_header">
-    <div id="product_page_container">
-        <img class="skateboardpic" src="/images/pexels-artem-podrez-4816757.jpg" alt="Skateboards">
-        <div class="centered">
-            <p class="smalltxt"><a href="welcome" class="homeLink">Home</a> > Product Page</p>
-            <h1 class="largetxt">Shop Page</h1>
-            <p class="mediumtxt">Let's build the skateboard you have always imagined.</p>
-        </div>
-    </div>
-</div> -->
+
 
 <div id="product_page_interface">
     <div id="product_page_interface_body">
