66fba7b17b981e8e19966feda160bc9394d62219
afarah2304 <230427239@aston.ac.uk>
2023-12-09 21:48:47 +0000

    Changed Username to show up in line with other links in header

--- CreativeCruisers/resources/views/header.blade.php
+++ CreativeCruisers/resources/views/header.blade.php
@@ -44,9 +44,9 @@
                     @endif
                     @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        <!-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                             data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}</a>
+                            {{ Auth::user()->name }}</a> -->
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
@@ -55,6 +55,7 @@
                             </form>
                         </div>
                     </li>
+                    <li><a href="{{ route('logout') }}"><h3>{{ Auth::user()->name }}</h3></a></li>
                     @endguest
 
 
