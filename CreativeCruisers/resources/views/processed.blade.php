2984fe1a319bec8477d3d44b12422cf3e676a75a
GurdevBopps <210073858@aston.ac.uk>
2024-03-06 23:15:52 +0000

    Update processed.blade.php

--- CreativeCruisers/resources/views/processed.blade.php
+++ CreativeCruisers/resources/views/processed.blade.php
@@ -18,57 +18,9 @@
     <link rel="stylesheet" href="{{ asset('css/nav_bar.css') }}">
     <link href="{{ asset('https://fonts.googleapis.com/css?family=Poppins') }}" rel='stylesheet'>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <div class="header">
 
-        <div class="navbar">
-
-            <div class="logo">
-                <img src="/images/creative_logo.png" width="125px">
-
-            </div>
-            <div class="title">
-                <p>Creative Cruisers</p>
-            </div>
-
-            <nav>
-                <ul>
-                    <li><a href="{{ route('admin.home') }}">Home</a></li>
-                    <li><a href="{{ route('admin_customerdetails') }}">Customer details</a></li>
-                    <li><a href="{{ route('home') }}">Main website</a></li>
-
-                    @guest
-                    @if (Route::has('login'))
-                    <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                    @endif
-                    @if (Route::has('register'))
-                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                    @endif
-                    @else
-                    <li class="nav-item dropdown">
-                        <!-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}</a> -->
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                    document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf
-                            </form>
-                        </div>
-                    </li>
-                    <li><a href="{{ route('admin.home') }}">
-                            <h3>{{ Auth::user()->name }}</h3>
-                        </a></li>
-                    @endguest
-
-                </ul>
-            </nav>
-
-
-        </div>
+      
 
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
